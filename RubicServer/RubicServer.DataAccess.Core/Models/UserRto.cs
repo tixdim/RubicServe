171b294c3a0626d2f6a4fd246fde34b0be854253
Zhuludkovr <tixdim.on1@gmail.com>
2021-11-13 17:50:28 +0300

    Обнова в бравле 1.1

---
+++ RubicServer/RubicServer.DataAccess.Core/Models/UserRto.cs
@@ -0,0 +1,10 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace RubicServer.DataAccess.Core.Models
+{   
+    [Table("User")]
+    public class UserRto
+    {
+        [key] public int id { get; set; }
+    }
+}
